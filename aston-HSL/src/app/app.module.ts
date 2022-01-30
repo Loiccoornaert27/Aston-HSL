acaa8a0f1fc9def82109bdbd3cb633e318df72c6
Loic Coornaert <71696114+Loiccoornaert27@users.noreply.github.com>
2022-01-31 00:13:22 +0100

    Ajout composant palette

--- aston-HSL/src/app/app.module.ts
+++ aston-HSL/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaletteComponent } from './components/palette/palette.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaletteComponent
   ],
   imports: [
     BrowserModule,
